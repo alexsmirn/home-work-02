847262e3c768897051087ef1edc1209b726b8566
alexsmirn <alexsmirnoffprogramming@gmail.com>
2024-03-23 21:35:51 +0200

    HW8 (Fixed Error)

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -8,12 +8,22 @@ export const homeWorkReducer = (state: UserType[], action: ActionType): any => {
     switch (action.type) {
         case 'sort': { // by name
             return action.payload === 'up'
-                ? [...state.sort((a, b) => b.age - a.age)]
-                : [...state.sort((a, b) => a.age - b.age)]
+                ? [...state.sort((a: any, b: any) => a.name.localeCompare(b.name))]
+                : [...state.sort((a: any, b: any) => b.name.localeCompare(a.name))]
+
+            // return action.payload === 'up'
+            //     ? [...state.sort((a, b) => a.age - b.age)]
+            //     : [...state.sort((a, b) => b.age - a.age)]
         }
 
         case 'check': {
-            return state.filter(el => el.age >= action.payload)
+
+            let filteredState = state.filter(el => el.age >= action.payload)
+            console.log(filteredState)
+            let sortedState = [...filteredState].sort((a: any, b: any) => a.name.localeCompare(b.name));
+            console.log(sortedState);
+
+            return sortedState
         }
 
         default:
